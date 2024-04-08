47822c8f43e1ffd43ac9910778d50cc5ea6a9a0e
zbynekslavicek <z.slavicek@email.cz>
2024-04-08 09:01:03 +0200

    msg

--- searching.py
+++ searching.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 # get current working directory path
@@ -12,6 +13,17 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+
+    with open(file_path, "r") as f:
+        allowed_key = json.load(f)
+
+    if field not in allowed_key:
+        return None
+
+    with open(file_name, "r") as f:
+        data = json.load(f)
+
+    return data.get(field)
 
 
 def main():
