061d0ac9c616721710d1d04c3f983d261693fc26
zbynekslavicek <z.slavicek@email.cz>
2025-04-10 12:27:25 +0200

    funkcedata

--- searching.py
+++ searching.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 # get current working directory path
 cwd_path = os.getcwd()
 
@@ -13,6 +13,20 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    allowed_fields = {"unordered_numbers", "ordered_numbers", "dna_sequence"}
+    if field not in allowed_fields:
+        return None
+
+    with open(file_path, "r") as file:
+        data = json.load(file)
+
+    return data[field]
+
+
+def linear_search(sekvence, hledane_cislo):
+    konecny_slovnik = {}
+
+
 
 def main():
     pass
@@ -20,3 +34,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+    numbers = read_data("sequential.json", field="unordered_numbers")
+    print(numbers)
